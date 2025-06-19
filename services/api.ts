b474797268a5d3ea6729ebaa8fb49052e56192f9
Prithviraj Patil <prithvirajpatil64@gmail.com>
2025-06-19 09:48:51 +0530

    added appwrite to backend

--- services/api.ts
+++ services/api.ts
@@ -26,17 +26,3 @@ export const fetchMovies = async ({ query }: { query: string}): Promise<Movie[]>
 
     return data.results;
 }
-
-// const url = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
-// const options = {
-//   method: 'GET',
-//   headers: {
-//     accept: 'application/json',
-//     Authorization: 'Bearer ${API KEY}'
-//   }
-// };
-
-// fetch(url, options)
-//   .then(res => res.json())
-//   .then(json => console.log(json))
-//   .catch(err => console.error(err));
